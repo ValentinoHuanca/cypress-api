3ff30a097edcc0fafc43341c306aba288dc72e6c
ValentinoHuanca <xdxdermano@gmail.com>
2024-08-23 16:10:05 -0300

    pass2

--- cypress/e2e/tests/api-test/Status.cy.js
+++ cypress/e2e/tests/api-test/Status.cy.js
@@ -1,12 +1,14 @@
-describe('',()=>{
+describe('TS|Status',()=>{
     it('test Api Status',()=>{
         cy.api({
             method:'GET',
             url:'/',
             qs:{}
         }).then(response =>{
+            const body = response.body
             expect(response).to.be.an('object')
             expect(response.status).to.eql(200)
+            expect(body.message).to.include("Welcome to the Simple Books API.")
         })
     })
 })
